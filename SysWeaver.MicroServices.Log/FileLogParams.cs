56b0abe283eeb3b44baa2d4fdc870f42129c6f50
Daniel Hansen <dh@dosh.se>
2025-10-22 15:36:36 +0200

    More fixes.

---
+++ SysWeaver.MicroServices.Log/FileLogParams.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace SysWeaver.MicroService
+{
+    public sealed class FileLogParams
+    {
+        /// <summary>
+        /// Optional filename for the log file (defaults to "$(ExecutableBase).log").
+        /// Can use path variables, ex:
+        ///             $(CommonApplicationData) = The directory that serves as a common repository for application-specific data that is used by all users.
+        ///             $(LocalApplicationData) = The directory that serves as a common repository for application-specific data that is used by the current, non-roaming user.
+        ///             $(ApplicationData) = The directory that serves as a common repository for application-specific data for the current roaming user (typically settings that should be shared between systems).
+        ///             $(MyPictures) = The My Pictures folder.
+        ///             $(AppName) = Application name.
+        ///             $(Executable) = Full path to the executable.
+        ///             $(ExecutableDir) = ExecutableDir.
+        ///             $(ExecutableBase) = Full path to the folder where the application is.
+        ///             $(Cc) = A "unique" id for this process
+        /// </summary>
+        public String Filename;
+        /// <summary>
+        /// How much detail to include in the log
+        /// </summary>
+        public Message.TextStyles Style = Message.TextStyles.Verbose;
+        /// <summary>
+        /// How to handle writing, 
+        /// </summary>
+        public MessageHandler.Modes Mode = MessageHandler.Modes.Async;
+        /// <summary>
+        /// The maximum size of the logfile, when exceeded it is truncated to half it's size.
+        /// So reliable half of this size is available.
+        /// </summary>
+        public long MaxSize = 2 << 20;
+    }
+
+}
---
+++ SysWeaver.MicroServices.Log/srcWeb/logFile/logfile.js
@@ -0,0 +1,62 @@
+﻿
+
+
+
+
+
+async function logFileMain() {
+
+    try {
+        const target = document.body;
+        const p = document.createElement("pre");
+        async function updateText() {
+            p.innerText = await getRequest("../Api/logFile/LogFile.txt", false, false, null, async e => e.text());
+        }
+        await updateText();
+        const user = await getRequest("../Api/auth/GetUser");
+        const haveAdmin = user && user.Succeeded && user.Tokens && (user.Tokens.indexOf("admin") >= 0);
+        const br = Button.CreateRow();
+        target.appendChild(br);
+        const d = new Button("", "Download", "Click to download the file", "IconDownload", true, async () => {
+            d.StartWorking();
+            try {
+                const name = await getRequest("../Api/logFile/DownloadName");
+                downloadFile(name, "../Api/logFile/LogFile.txt");
+            }
+            catch (e) {
+                Fail("Failed to download log file:\n" + e);
+            }
+            d.StopWorking();
+        });
+        br.appendChild(d.Element);
+        if (haveAdmin) {
+            const del = new Button("", "Delete", "Click to delete the log file from the disc on the server", "IconDelete", true, async () => {
+                del.StartWorking();
+                if (await Confirm("Delete log",
+                    "The file will be deleted from the disc on the server.\n\nAre you sure you want to delete it?",
+                    "Yes, delete!", "No, keep it!", "IconDelete")) {
+                    try {
+                        if (await getRequest("../Api/logFile/DeleteLogFile")) {
+                            Info("Log file deleted from disc!");
+                            await updateText();
+
+                        } else {
+                            Fail("Failed to delete log file!");
+                        }
+                    }
+                    catch (e) {
+                        Fail("Failed to delete log file:\n" + e);
+                    }
+                };
+                del.StopWorking();
+            });
+            br.appendChild(del.Element);
+
+        }
+        target.appendChild(p);
+    }
+    catch (e) {
+        Fail(e);
+    }
+    PageLoaded();
+}
