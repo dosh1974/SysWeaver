5cf2444a2266ed9b7c4a9acccdc100b908c5bb00
SolidEq <dh@solidicon.com>
2026-08-28 16:41:36 +0200

    Added a small utility class.

--- SysWeaver.Common/TaskExt.cs
+++ SysWeaver.Common/TaskExt.cs
@@ -50,7 +50,7 @@ namespace SysWeaver
         /// </summary>
         /// <param name="task">A function that creates the new task, and then returns the result of ConfigureAwait(false) on it</param>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static void StartNewAsyncChain(this Func<ValueTask> task) => Task.Run(task);
+        public static void StartNewAsyncChainValue(this Func<ValueTask> task) => Task.Run(task);
 
         /// <summary>
         /// Run a task in a new thread / chain, then wait for the task to complete and return it's value
@@ -1516,5 +1516,70 @@ namespace SysWeaver
         public static ValueTask WhenAll(params ValueTask[] tasks)
             => WhenAll(tasks as IReadOnlyList<ValueTask>);
 
+
     }
+
+
+
+    /// <summary>
+    /// A class than let's you spawn an async task.
+    /// Will wait for any previous task to complete before starting the new one.
+    /// If a previous task throws an exception, the Start method will throw that excpetion.
+    /// </summary>
+    public sealed class SingleTaskRunner
+    {
+        readonly AsyncLock Lock = new AsyncLock();
+
+        Exception Ex;
+
+
+        /// <summary>
+        /// Wait until any previous task is completed.
+        /// If a previous task threw an exception, this method will throw that exception.
+        /// </summary>
+        /// <returns></returns>
+        public async Task Wait()
+        {
+            using var d = await Lock.Lock().ConfigureAwait(false);
+            var e = Interlocked.Exchange(ref Ex, null);
+            if (e != null)
+                throw e;
+        }
+
+        /// <summary>
+        /// Wait until any previous task is completed.
+        /// If a previous task threw an exception, this method will throw that exception.
+        /// Start the supplied task ina new async chain, and return immediately.
+        /// </summary>
+        /// <param name="task"></param>
+        /// <returns></returns>
+        public async Task Start(Func<Task> task)
+        { 
+            var d = await Lock.Lock().ConfigureAwait(false);
+            var e = Interlocked.Exchange(ref Ex, null);
+            if (e != null)
+            {
+                d.Dispose();
+                throw e;
+            }
+            TaskExt.StartNewAsyncChain(async () =>
+            {
+                try
+                {
+                    await task().ConfigureAwait(false); 
+                }
+                catch (Exception ex)
+                {
+                    Interlocked.Exchange(ref Ex, ex);
+                }
+                finally
+                {
+                    d.Dispose();
+                }
+            });
+        }
+
+
+    }
+
 }
